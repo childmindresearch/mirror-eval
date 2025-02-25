a54c7b8de69b93fc0124e7f6080b5fce51ed70c3
reindervdw-cmi <reinder.vosdewael@childmind.org>
2025-02-25 13:53:19 -0500

    feat: Add support for running a LLM statement metric. (#6)

--- src/mirror_eval/experiments/opik_capabilities/custom_metric.py
+++ src/mirror_eval/experiments/opik_capabilities/custom_metric.py
@@ -23,3 +23,23 @@ def run_custom_metric_experiment() -> None:
         model="gpt-3.5-turbo",
         scoring_metrics=[query_metric],
     )
+
+
+def run_statement_metric_experiment() -> None:
+    """Experiment to test statement metrics in OPIK."""
+    client = service.get_client()
+    dataset = client.get_dataset("RAPI_test")
+    statement_metric = metric.LlmStatementMetric(
+        ["The text is in French.", "The text is in English."], "gpt-4"
+    )
+    evaluation.evaluate_prompt(
+        dataset=dataset,
+        messages=[
+            {
+                "role": "user",
+                "content": "Translate the following text to French: {{text}}",
+            }
+        ],
+        model="gpt-3.5-turbo",
+        scoring_metrics=[statement_metric],
+    )
