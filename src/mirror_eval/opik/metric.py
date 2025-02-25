1e69826a3b8d9c9e03577d113382657122c36fda
reinder.vosdewael@childmind.org <reinder.vosdewael@childmind.org>
2025-02-25 12:31:49 -0500

    feat: Add support for running a LLM statement metric.

--- src/mirror_eval/opik/metric.py
+++ src/mirror_eval/opik/metric.py
@@ -2,6 +2,7 @@
 
 import json
 import re
+import statistics
 from typing import Any
 
 import opik
@@ -171,4 +172,142 @@ class RegexMetric(base_metric.BaseMetric):
         return score_result.ScoreResult(
             name=self.name,
             value=float(bool(match)),
+        )
+
+
+class LlmStatementMetric(base_metric.BaseMetric):
+    """A float metric based on True/False statements evaluated by an LLM."""
+
+    def __init__(
+        self,
+        statements: list[str],
+        model: str | base_model.OpikBaseModel | None = None,
+        name: str = "Statement Model",
+        *,
+        track: bool = True,
+    ) -> None:
+        """Initialize the query metric.
+
+        Args:
+            statements: The statements to evaluate.
+            model: The model to use in metric computation.
+            name: The name of the metric.
+            track: Whether to track the metric. Defaults to True.
+        """
+        super().__init__(name=name, track=track)
+        self._statements = statements
+        self._statements_tracked = False
+        self._name = name
+
+        if isinstance(model, base_model.OpikBaseModel):
+            self._model = model
+        else:
+            self._model = models_factory.get(model_name=model)
+
+    @opik.track()
+    def _track_statements(self, _statements: list[str]) -> None:
+        """Workaround to insert the statements into the tracking system.
+
+        Args:
+            _statements: The statements to track.
+        """
+        self._statements_tracked = True
+
+    def score(
+        self,
+        input: str,  # noqa: A002
+        output: str,
+        **_ignored_kwargs: Any,  # noqa: ANN401
+    ) -> score_result.ScoreResult:
+        """Calculate the score for the given input and output.
+
+        Args:
+            input: The original input/question.
+            output: The LLM's output to evaluate.
+            **_ignored_kwargs: Additional keyword arguments that are ignored.
+
+        Returns:
+            A ScoreResult with 1 if a match was found, 0 otherwise.
+        """
+        if not self._statements_tracked:
+            self._track_statements(self._statements)
+
+        model_output = self._model.generate_string(
+            input=self._get_input(input, output),
+            response_format=self._get_response_model(),
+        )
+        return self._parse_model_output(model_output)
+
+    async def ascore(
+        self,
+        input: str,  # noqa: A002
+        output: str,
+        **_ignored_kwargs: Any,  # noqa: ANN401
+    ) -> score_result.ScoreResult:
+        """Calculate the score for the given input and output.
+
+        Args:
+            input: The original input/question.
+            output: The LLM's output to evaluate.
+            **_ignored_kwargs: Additional keyword arguments that are ignored.
+
+        Returns:
+            A ScoreResult with 1 if a match was found, 0 otherwise.
+        """
+        if not self._statements_tracked:
+            self._track_statements(self._statements)
+
+        model_output = await self._model.agenerate_string(
+            input=self._get_input(input, output),
+            response_format=self._get_response_model(),
+        )
+        return self._parse_model_output(model_output)
+
+    def _get_input(self, input: str, output: str) -> str:  # noqa: A002
+        """Gets the input prompt for scoring.
+
+        Returns:
+            The input prompt.
+        """
+        statement_prompt = "- " + "\n- ".join(self._statements)
+        return template.statements_base_prompt.format(
+            input=input, output=output, statements=statement_prompt
+        )
+
+    def _get_response_model(self) -> object:
+        """Creates a response model from the statements.
+
+        This must be done dynamically to ensure that the 'statement'
+        property is restricted to a copy of the input.
+
+        Returns:
+            The response model.
+        """
+        statement_models = []
+        for statement in self._statements:
+            statement_pattern = "^" + statement + "$"
+
+            class Statement(pydantic.BaseModel):
+                statement: str = pydantic.Field(..., pattern=statement_pattern)
+                evaluation: str = pydantic.Field(..., min_length=20)
+                conclusion: bool
+
+            statement_models.append(Statement)
+
+        class StatementModel(pydantic.BaseModel):
+            statements: tuple[*statement_models]  # type: ignore[valid-type]
+
+        return StatementModel
+
+    def _parse_model_output(self, content: str) -> score_result.ScoreResult:
+        """Extracts the scores from the LLM response.
+
+        Returns:
+            A ScoreResults wherein the value is the average score.
+        """
+        dict_content = json.loads(content)
+        score = statistics.mean(response["conclusion"] for response in dict_content)
+        return score_result.ScoreResult(
+            name=self._name,
+            value=score,
         )
